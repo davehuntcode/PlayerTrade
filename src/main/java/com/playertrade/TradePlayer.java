a8041c02d52990e71be62d89cc2fcec78bfec31a
Dave <thedavidhunt@hotmail.com>
2012-08-14 15:07:15 -0400

    Code  cleanup

--- src/main/java/com/playertrade/TradePlayer.java
+++ src/main/java/com/playertrade/TradePlayer.java
@@ -10,63 +10,61 @@ import org.bukkit.inventory.ItemStack;
 
 public class TradePlayer {
     public static List<TradePlayer> players = new ArrayList<TradePlayer>();
-    
+
     public Player self;
-    
+
     public TradePlayer inTradeWith;
-    
+
     public boolean trading;
     public boolean confirmed;
-    
+
     public int minSlot;
     public int maxSlot;
-    
+
     public TradePlugin plugin;
 
     public TradePlayer(TradePlugin plugin, Player player) {
 	this.plugin = plugin;
 	this.self = player;
     }
-    
-    
+
     public boolean hasPermission(String perm) {
 	return plugin.hasPermission(self.getName(), perm);
     }
 
-
     public void showTradeWindow(TradingScreen tradeWindow) {
 	self.openInventory(tradeWindow);
     }
-    
+
     public void showInv(Inventory tradeWindow) {
-  	self.openInventory(tradeWindow);
-      }
-    
+	self.openInventory(tradeWindow);
+    }
+
     public void closeTradeWindow() {
 	self.closeInventory();
     }
-    
+
     public void cancelTrade() {
-   	this.closeTradeWindow();
-   	TradePlayer.players.remove(inTradeWith);
-   	this.inTradeWith = null;
-   	this.setTrading(false);
-   	
-       }
-    
+	this.closeTradeWindow();
+	TradePlayer.players.remove(inTradeWith);
+	this.inTradeWith = null;
+	this.setTrading(false);
+
+    }
+
     public static TradePlayer getTradePlayer(Player p) {
 	return getTradePlayer(p.getName());
     }
-    
+
     public static TradePlayer getTradePlayer(String pName) {
-	for(TradePlayer tp : players) {
-	    if(tp.self.getName().equals(pName)) {
+	for (TradePlayer tp : players) {
+	    if (tp.self.getName().equals(pName)) {
 		return tp;
 	    }
 	}
 	return null;
     }
-    
+
     public void sendMessage(String m) {
 	self.sendMessage(m);
     }
@@ -79,19 +77,20 @@ public class TradePlayer {
 	return trading;
     }
 
-
     public void handleInventoryClick(InventoryClickEvent e) {
 	try {
 	    int slot = e.getSlot();
 	    ItemStack i = e.getCurrentItem();
 	    TradePlayer other = null;
-	    if(this.inTradeWith != null) other = this.inTradeWith;
-	    
-	    if( (slot > minSlot) && (slot < maxSlot) ) {
-		if(this.hasConfirmedTrade()) {
-		    
+	    if (this.inTradeWith != null)
+		other = this.inTradeWith;
+
+	    if ((slot > minSlot) && (slot < maxSlot)) {
+		if (this.hasConfirmedTrade()) {
+
 		} else {
-		    plugin.info(String.format("%s has added/removed %s", e.getWhoClicked().getName(), i.getType().name()));
+		    plugin.info(String.format("%s has added/removed %s", e
+			    .getWhoClicked().getName(), i.getType().name()));
 		    e.setCancelled(true);
 		}
 	    }
@@ -100,23 +99,20 @@ public class TradePlayer {
 	}
     }
 
-
     public boolean hasConfirmedTrade() {
 	// TODO Auto-generated method stub
 	return false;
     }
 
-
     public TradePlayer getRequested() {
 	return inTradeWith;
     }
-
 
     public void registerTrade(TradePlayer other) {
 	players.add(other);
 	this.setTrading(true);
 	this.inTradeWith = other;
-	
+
     }
 
 }
